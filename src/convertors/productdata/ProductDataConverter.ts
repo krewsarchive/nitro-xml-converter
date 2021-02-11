4b7e35bd00a9b708b6570722091e135866425775
Bill <billsonnn@live.com>
2021-02-10 19:57:47 -0500

    Add productdata converter

---
+++ src/convertors/productdata/ProductDataConverter.ts
@@ -0,0 +1,91 @@
+import { existsSync, readFile, writeFile } from 'fs';
+import { Converter } from '../../common/Converter';
+
+export class ProductDataConverter extends Converter
+{
+    private _productCount: number;
+    private _products: [string, string, string][] = [];
+
+    constructor()
+    {
+        super();
+
+        this._productCount  = 0;
+        this._products      = [];
+    }
+
+    public convert(): void
+    {
+        if(!existsSync('input/productdata.txt'))
+        {
+            this.logger.warn(`Skipping, input/productdata.txt doesn't exist!`);
+
+            return;
+        }
+
+        super.convert();
+
+        this.logger.log(`Running`);
+
+        readFile('input/productdata.txt', 'utf8', (err, data) =>
+        {
+            if(err)
+            {
+                this.logger.error(err.message, err.stack);
+
+                return;
+            }
+
+            this.parseProductData(data);
+            
+            this.generateJson();
+        });
+    }
+
+    public parseProductData(data: string): void
+    {
+        if(!data) return;
+
+        data = data.replace(/"{1,}/g, '');
+
+        const parts = data.split(/\n\r{1,}|\n{1,}|\r{1,}/mg);
+
+        for(const part of parts)
+        {
+            const set = part.match(/\[+?((.)*?)\]/g);
+
+            for(const entry of set)
+            {
+                let value = entry.replace(/\[{1,}/mg, '');
+                value = entry.replace(/\]{1,}/mg, '');
+
+                value = value.replace('[[', '');
+                value = value.replace('[', '');
+
+                const pieces        = value.split(',');
+                const productCode   = pieces.shift();
+                const name          = pieces.shift();
+                const description   = pieces.join(',');
+
+                this._products.push([ productCode, name, description ]);
+
+                this._productCount++;
+            }
+        }
+    }
+
+    public generateJson(): void
+    {
+        writeFile('output/ProductData.json', JSON.stringify(this._products), (err) =>
+        {
+            if(err)
+            {
+                this.logger.error(err.message, err.stack);
+
+                return;
+            }
+
+            this.logger.log(`Finished with ${ this._productCount } items in ${ Date.now() - this.startTime }ms`);
+        });
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { EffectMapConverter } from './convertors/effectmap/EffectMapConverter';
 import { ExternalTextsConverter } from './convertors/externals/ExternalTextsConverter';
 import { FigureMapConverter } from './convertors/figuremap/FigureMapConverter';
 import { FurniDataConverter } from './convertors/furnidata/FurniDataConverter';
+import { ProductDataConverter } from './convertors/productdata/ProductDataConverter';
 
 if(!existsSync('output')) mkdirSync('output');
 
@@ -10,4 +11,5 @@ new FigureMapConverter().convert();
 new EffectMapConverter().convert();
 new FurniDataConverter().convert();
 new ExternalTextsConverter().convert();
+new ProductDataConverter().convert();
 //new RoomContentConverter().convert();
